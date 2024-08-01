74b619ed4dc3db14a6502cf04f95d00c18d42660
damarbob <ibn.damr@gmail.com>
2024-08-01 07:42:38 +0700

    Improved admin gallery

--- app/Models/GaleriModel.php
+++ app/Models/GaleriModel.php
@@ -17,20 +17,44 @@ class GaleriModel extends \CodeIgniter\Model
         return $this->orderBy('created_at', 'DESC')->findAll();
     }
 
+    // public function getPaginated($page, $perPage, $search = '')
+    // {
+    //     $offset = ($page - 1) * $perPage;
+
+    //     // Build the query
+    //     $builder = $this->table($this->table);
+
+    //     if (!empty($search)) {
+    //         // Apply search filtering if search term is provided
+    //         $builder->like('judul', $search)
+    //             ->orLike('alt', $search);
+    //     }
+
+    //     return $builder->orderBy('created_at', 'DESC') // Optional: order by created_at or another field
+    //         ->findAll($perPage, $offset);
+    // }
+
     public function getPaginated($page, $perPage, $search = '')
     {
         $offset = ($page - 1) * $perPage;
 
-        // Build the query
         $builder = $this->table($this->table);
 
         if (!empty($search)) {
-            // Apply search filtering if search term is provided
             $builder->like('judul', $search)
                 ->orLike('alt', $search);
         }
 
-        return $builder->orderBy('created_at', 'DESC') // Optional: order by created_at or another field
+        $total = $builder->countAllResults(false);
+
+        $data = $builder->orderBy('created_at', 'DESC')
             ->findAll($perPage, $offset);
+
+        return [
+            'data' => $data,
+            'total' => $total,
+            'perPage' => $perPage,
+            'currentPage' => $page
+        ];
     }
 }
